3278e3ddef305d470a66dfe270bc1dfdf0ece25a
Herve Meyer <meyerherve.web@outlook.fr>
2018-11-12 11:36:30 +0100

    protocol REQ_USE_TICKET

--- fr.ensisa.hassenforder.transportation.empty/fr.ensisa.hassenforder.transportation.common.empty/src/fr/ensisa/hassenforder/transportation/terminal/network/Protocol.java
+++ fr.ensisa.hassenforder.transportation.empty/fr.ensisa.hassenforder.transportation.common.empty/src/fr/ensisa/hassenforder/transportation/terminal/network/Protocol.java
@@ -5,12 +5,9 @@ public interface Protocol {
     public static final int TERMINAL_PORT = 8888;
 
     public static final int REQ_FETCH = 1000;
-
-    public static final int REQ_USE_TICKET = 1002;
+    public static final int REQ_USE_TICKET = 1001;
 
     public static final int REPLY_PASS = 2000;
-
     public static final int REPLY_OK = 2001;
-
     public static final int REPLY_KO = 2002;
 }
