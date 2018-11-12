0372083f2b5ad8fbb4f735276c5b7ca56a126215
Herve Meyer <meyerherve.web@outlook.fr>
2018-11-12 11:45:22 +0100

    Kiosk protocol

--- fr.ensisa.hassenforder.transportation.empty/fr.ensisa.hassenforder.transportation.common.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/Protocol.java
+++ fr.ensisa.hassenforder.transportation.empty/fr.ensisa.hassenforder.transportation.common.empty/src/fr/ensisa/hassenforder/transportation/kiosk/network/Protocol.java
@@ -3,5 +3,21 @@ package fr.ensisa.hassenforder.transportation.kiosk.network;
 public interface Protocol {
 
     public static final int KIOSK_PORT = 7777;
-    
+
+    public static final int REQ_FETCH = 1000;
+    public static final int REQ_NEW_CARD = 1001;
+
+    public static final int REQ_BUY_ROUTE = 1002;
+    public static final int REQ_BUY_URBAN = 1003;
+    public static final int REQ_BUY_SUBSCRIPTION = 1004;
+
+    public static final int REQ_PAY = 1005;
+    public static final int REQ_CANCEL = 1006;
+
+
+    public static final int REPLY_OK = 2000;
+    public static final int REPLY_KO = 2001;
+
+    public static final int REPLY_PASS = 2002;
+    public static final int REPLY_PRICE = 2003;
 }
